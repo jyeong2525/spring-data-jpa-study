b3c1545c4c7dc307e05fb400f5d248fe85b6f24d
jyeong2525 <yhwan0226@gmail.com>
2024-08-08 17:33:04 +0900

    feat: 쿼리메소드 기능 JPA직접 사용 JPA NamedQuery

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -4,9 +4,14 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"})
+@NamedQuery(
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id
