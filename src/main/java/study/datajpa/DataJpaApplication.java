3a390abea07d8353907e6bccda9c4b3fa43f0fd6
jyeong2525 <yhwan0226@gmail.com>
2024-08-09 17:19:15 +0900

    feat: 스프링 데이터 JPA Auditing 엔티티를 생성, 변경 시 변경한 사람과 시간을 추적하고 싶을 때 - 등록자, 수정자

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DataJpaApplication {
 
     public static void main(String[] args) {
