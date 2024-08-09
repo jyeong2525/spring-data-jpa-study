2be2d4b75cb8941a44ca8c339f4ab5276a68a715
jyeong2525 <yhwan0226@gmail.com>
2024-08-09 17:14:29 +0900

    feat: 순수 JPA Auditing 엔티티를 생성, 변경 시 변경한 사람과 시간을 추적하고 싶을 때

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -13,7 +13,7 @@ import lombok.*;
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"})
-public class Member {
+public class Member extends JpaBaseEntity{
 
     @Id
     @GeneratedValue
